be89fa6448d4ff1e2ded35f65c0df8f349c98be5
Hardik Makwana <hardik19998@gmail.com>
2021-03-27 12:05:20 +0530

    Added validation to form

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  todos: Todo[] = [
+    new Todo('First todo item', true),
+    new Todo('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,')
+  ];
+
+  constructor() { }
+
+  getAllTodos() {
+    return this.todos;
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  updateTodo(index: number, updatedTodo: Todo) {
+    this.todos[index] = updatedTodo;
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+}
