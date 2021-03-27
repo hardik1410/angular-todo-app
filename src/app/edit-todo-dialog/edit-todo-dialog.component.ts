d9812fa5ca21b0f70c8393d8f8443d0f0a4d9eb2
Hardik Makwana <hardik19998@gmail.com>
2021-03-27 17:37:54 +0530

    Added CRUD and directives for tooltips

---
+++ src/app/edit-todo-dialog/edit-todo-dialog.component.ts
@@ -0,0 +1,38 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Todo } from '../shared/todo.model';
+
+@Component({
+  selector: 'app-edit-todo-dialog',
+  templateUrl: './edit-todo-dialog.component.html',
+  styleUrls: ['./edit-todo-dialog.component.scss']
+})
+export class EditTodoDialogComponent implements OnInit {
+
+  showValidationErrors: boolean = false;
+
+  constructor(public dialogRef: MatDialogRef<EditTodoDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public todo: Todo) { }
+
+  ngOnInit(): void {
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  onFormSubmit(form: NgForm) {
+    if(form.invalid)
+      return this.showValidationErrors = true
+    
+    const updatedTodo = {
+      ...this.todo,
+      ...form.value
+    }
+    this.showValidationErrors = false
+    this.dialogRef.close(updatedTodo)
+    return
+  }
+
+}
